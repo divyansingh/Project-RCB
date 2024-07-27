19bab89d92f535aa8cffb0175d8da3e53d6b1ffe
Divyansh Singh <singh.divyansh1@indiamart.com>
2024-07-28 00:44:55 +0530

    dummy resume ui

---
+++ resume_maker/src/components/resume/InputDashboard.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import profileIcon from '../../imageUtils/student.png'
+import educationIcon from '../../imageUtils/education.png'
+import workIcon from '../../imageUtils/work-experience.png'
+import infoIcon from '../../imageUtils/info-button.png'
+import printIcon from '../../imageUtils/printer-icon.png'
+import './css/inputDashboard.css'
+const InputDashboard = () => {
+  return (
+      <div className='df fdc fwNW jcse inputDashboard'>
+        <DasboardElements path={profileIcon} alt={'Profile'} />
+        <DasboardElements path={educationIcon} alt={'Profile'} />
+        <DasboardElements path={workIcon} alt={'Profile'} />
+        <DasboardElements path={infoIcon} alt={'Profile'} />
+        <DasboardElements path={printIcon} alt={'Profile'} />
+      </div>
+  )
+}
+
+export default InputDashboard
+
+const DasboardElements = ({ path, alt }) => {
+  return (
+    <div>
+      <img src={path} alt={alt} style={{ height: 50, width: 50 }} />
+    </div>
+  )
+}
--- resume_maker/src/components/resume/Landing.js
+++ resume_maker/src/components/resume/Landing.js
@@ -1,9 +1,27 @@
 import React from 'react'
-
-const Landing=()=> {
-  return (
-    <div>Landing</div>
-  )
+import classes from './css/landing.module.css'
+import InputDashboard from './InputDashboard'
+const Landing = () => {
+    return (
+        <div className='df'>
+            <InputDashboard/>
+            <div className={classes.outerContainer}>
+                <div className={classes.resumeContainer}>
+                    <ResumeSection title="Professional Summary" />
+                    <ResumeSection title="Employment History" />
+                    <ResumeSection title="Education" />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Landing
+const ResumeSection = ({ title }) => {
+    return (
+        <div className={classes.section}>
+            <h2>{title}</h2>
+            <div className={classes.line}></div>
+        </div>
+    );
+};
